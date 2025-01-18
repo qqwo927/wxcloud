6709e54a4b5754b1eb7ebcf673751bc3f8db4444
qqwo927 <52652319+qqwo927@users.noreply.github.com>
2025-01-19 01:02:24 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
+from flask import Flask, request, jsonify
+import os
+app= Flask(__name__)
 
+uploadfolder='uploads/'
+if not os.path.exists(uploadfolder):
+    os.makedirs(uploadfolder)
+
+@app.route('/upload',methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error':'No file part'})
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error':'No selected file'})
+    file.save(os.path.join(upload_file,file.filename))
+    return jsonify({'message':'File uploaded','filename':file.filename})
+if __name__ =='__main__':
+    app.run(debug=True)
